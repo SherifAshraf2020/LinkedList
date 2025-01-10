a30a704fc43a15a67028e029549c83f17c16de6d
Sherif <sherif.ashraf.farooq@gmail.com>
2025-01-10 21:13:39 +0200

    Add circular linked list and update multiple modified files

--- circularLinkedList/include/circularLinkedList.h
+++ circularLinkedList/include/circularLinkedList.h
@@ -1,6 +1,10 @@
 #ifndef CIRCULARLINKEDLIST_H
 #define CIRCULARLINKEDLIST_H
 
+#include <iostream>
+
+using namespace std;
+
 
 template <class Type>
 struct nodeType
@@ -15,7 +19,7 @@ class circularLinkedList
 {
     public:
         circularLinkedList();
-        circularLinkedList();
+        circularLinkedList(const circularLinkedList<Type>& otherList);
         circularLinkedList& operator=(const circularLinkedList<Type>& otherList);
 
         void initializeList();
@@ -35,6 +39,55 @@ class circularLinkedList
     private:
         void copyList(const circularLinkedList<Type>& otherList);
 };
+
+
+template <class Type>
+circularLinkedList<Type>::circularLinkedList()
+{
+    first = NULL;  // Initialize the list as empty
+    count = 0;     // Set the count to 0
+}
+
+
+
+template <class Type>
+circularLinkedList<Type>::circularLinkedList(const circularLinkedList<Type>& otherList)
+{
+    copyList(otherList);  // Use the copyList function to copy nodes
+}
+
+
+template <class Type>
+circularLinkedList<Type>& circularLinkedList<Type>::operator=(const circularLinkedList<Type>& otherList)
+{
+    if (this != &otherList)     // Avoid self-assignment
+    {
+        destroy();               // Destroy the current list
+        copyList(otherList);     // Copy the other list's contents
+    }
+    return *this;                // Return the current object
+}
+
+
+
+
+template <class Type>
+void circularLinkedList<Type>::copyList(const circularLinkedList<Type>& otherList)
+{
+    first = NULL;  // Start with an empty list
+    count = 0;     // Initialize the count to 0
+
+    // If the other list is empty, we're done
+    if (otherList.first == NULL) {
+        return;
+    }
+
+    nodeType<Type> *current = otherList.first->link;  // Start from the first node of the other list
+    do {
+        insert(current->info);  // Insert each element from the other list into this list
+        current = current->link;
+    } while (current != otherList.first->link);  // Continue until we circle back to the start
+}
 
 template <class Type>
 bool circularLinkedList<Type>::isEmpty() const
@@ -57,68 +110,107 @@ int circularLinkedList<Type>::length() const
 template <class Type>
 void circularLinkedList<Type>::destroy()
 {
-    nodeType<Type> *temp;
-    temp = first->link;
+   if (first == NULL) return; // If the list is empty, nothing to destroy.
 
-    while(temp != first)
+    nodeType<Type> *current = first->link; // Start from the node after `first`.
+    nodeType<Type> *temp;
+
+    while (current != first) // Traverse until we delete all nodes except the one pointed to by `first`.
     {
-        first->link = temp->link;
-        delete temp;
-        temp = first->link;
+        temp = current;
+        current = current->link; // Move to the next node.
+        delete temp;             // Delete the current node.
     }
 
-    delete temp;
-    first = NULL;
-    count = 0;
+    // Delete the node pointed to by `first`.
+    delete first;
+    first = NULL; // Reset `first` to NULL.
+    count = 0;    // Reset the count.
 }
 
 template <class Type>
 void circularLinkedList<Type>::print() const
 {
-    nodeType<Type> *current;
-    current = first->link;
+    if (first == NULL) // Check if the list is empty.
+    {
+        cout << "List is empty" << endl;
+        return;
+    }
+
+
+    nodeType<Type> *current = first->link; // Start from the actual first node.
 
     do
     {
-        cout<<current->info<<" ";
-        current = current->link;
+        cout << current->info << " "; // Print the current node's info.
+        current = current->link;     // Move to the next node.
+    } while (current != first->link); // Stop when we circle back to the start.
 
-    }while(current != first->link);
-
+    cout << endl; // Add a newline at the end.
 }
 
 template <class Type>
 bool circularLinkedList<Type>::search(const Type& searchItem) const
 {
-    bool found = false;
-    nodeType<Type> *current;
+    if (first == NULL) // If the list is empty, return false.
+        return false;
 
-    current = first->link;
+    nodeType<Type> *current = first->link; // Start from the actual first node.
 
     do
     {
-        if(current->info == searchItem)
-        {
-            found = true;
-        }
-        else
-        {
-            current = current->link;
-        }
+        if (current->info >= searchItem) // Exit the loop if current info is >= searchItem
+            break;
 
-    }while(current != first->link && !found);
+        current = current->link; // Move to the next node.
+    } while (current != first->link); // Continue until we circle back to the starting node.
 
-    if(found)
-    {
-        found = (current->info == searchItem);
-    }
-    return found;
+    // After exiting the loop, check if we found the item.
+    return (current->info == searchItem); // If we find the searchItem, return true.
 }
 
 template <class Type>
 void circularLinkedList<Type>::insert(const Type& insertItem)
 {
+    nodeType<Type>* newNode = new nodeType<Type>;
+    newNode->info = insertItem;
 
+    if(isEmpty())
+    {
+        newNode->link = newNode;
+        first = newNode;
+    }
+    else
+    {
+        nodeType<Type>* current = first->link; // Start from the first actual node
+        nodeType<Type>* trailCurrent = first;  // Start from the last node
+
+        if(newNode->info < current->info)
+        {
+            newNode->link = current;
+            trailCurrent->link =newNode;
+            first->link = newNode;
+        }
+        else
+        {
+             do
+             {
+                 trailCurrent = current;
+                 current = current->link;
+             }while(current != first->link && current->info < newNode->info);
+
+             trailCurrent->link = newNode;
+             newNode->link = current;
+
+             if(trailCurrent == first)
+             {
+                 first = newNode;
+             }
+        }
+
+    }
+
+    count++;
 }
 
 
@@ -126,5 +218,63 @@ template <class Type>
 void circularLinkedList<Type>::deleteNode(const Type& deleteItem)
 {
 
+    if (first == NULL)
+    {  // Case 1: The list is empty
+        cout << "Cannot delete from an empty list." << endl;
+        return;
+    }
+
+    nodeType<Type> *current = first->link;  // Start from the first node
+    nodeType<Type> *trailCurrent = first;   // Points to the last node initially
+    bool found = false;
+
+    // Traverse the list to find the node to delete
+    do
+    {
+        if (current->info == deleteItem)
+        {
+            found = true;
+            break;
+        }
+        trailCurrent = current;
+        current = current->link;
+    } while (current != first->link);
+
+    if (!found) // Case 2: The item is not in the list
+    {
+        cout << "The item to be deleted is not in the list." << endl;
+        return;
+    }
+
+    // Case 3: Delete the node
+    if (current == first->link)
+    {  // If deleting the first actual node
+        first->link = current->link;  // Update first to point to the next node
+        if (first->link == current)
+        { // If there's only one node
+            first = NULL;  // Reset the list to empty
+        }
+    }
+    else    // Delete a middle or last node
+    {
+        trailCurrent->link = current->link;
+
+        if (current == first)   // If deleting the last node
+        {
+            first = trailCurrent;  // Update first to point to the new last node
+        }
+    }
+
+    delete current;  // Free the memory
+    count--;         // Decrement the node count
+
 }
+
+
+template <class Type>
+circularLinkedList<Type>::~circularLinkedList()
+{
+    destroy();  // Calls the destroy function to delete all nodes
+}
+
 #endif // CIRCULARLINKEDLIST_H
--- circularLinkedList/main.cpp
+++ circularLinkedList/main.cpp
@@ -1,9 +1,49 @@
 #include <iostream>
+#include "circularLinkedList.h"
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+     circularLinkedList<int> list;
+
+    std::cout << "Inserting elements into the list: 30, 10, 40, 20, 50\n";
+
+    list.insert(30);
+    list.insert(10);
+    list.insert(40);
+    list.insert(20);
+    list.insert(50);
+
+    std::cout << "List after insertion (should be sorted): ";
+    list.print();
+
+    std::cout << "Length of the list: " << list.length() << "\n";
+
+    int searchItem = 60;
+    std::cout << "Searching for " << searchItem << ": "
+              << (list.search(searchItem) ? "Found" : "Not Found") << "\n";
+
+    int deleteItem = 30;
+    std::cout << "Deleting item " << deleteItem << " from the list...\n";
+    list.deleteNode(deleteItem);
+
+    std::cout << "List after deletion: ";
+    list.print();
+
+    std::cout << "Creating a copy of the list (list2)...\n";
+    circularLinkedList<int> list2 = list;
+
+    std::cout << "List2 after assignment from list: ";
+    list2.print();
+
+    std::cout << "Destroying list...\n";
+    list.destroy();
+
+    std::cout << "List after destruction: ";
+    list.print();
+
 }
+
+
+
